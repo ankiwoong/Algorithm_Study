1bac2dc6816267f3ee54bb6143f0979c6f022225
AnKiWoong <ankiwoong@gmail.com>
2020-02-24 12:28:59 +0900

    master

--- Basic_Sort/Bubble_Sort.py
+++ Basic_Sort/Bubble_Sort.py
@@ -1,21 +1,32 @@
 '''
 버블 정렬?
-두 인접한 데이터를 비교해서, 앞에 있는 데이터가 뒤에 잇는 데이터보다 크면, 자리를 바구는 정렬 알고리즘
+두 인접한 데이터를 비교해서, 앞에 있는 데이터가 뒤에 잇는 데이터보다 크면, 자리를 바꾸는 정렬 알고리즘
 
 데이터길이      조건체크      턴
 2               1             1
 3               2             2
 4               3             3
 
-for index in range(데이터 길이 - 1):
-    for index2 in range(데이터길이 - 1):
-        if 앞데이터 > 뒤데이터:
-            swap(앞데이터, 뒤데이터)
+for index in range(데이터 길이 - 1):            # 턴
+    for index2 in range(데이터 길이 - 1):       # 조건 체크
+        if 앞 데이터 > 뒤 데이터:               # 조건 비교 후 변경 
+            데이터 교체(앞 데이터, 뒤 데이터)   # 앞 데이터와 뒤 데이터 변경
+
+
+for index in range(데이터 길이 - 1):            # 턴
+    swap = False                                # 데이터를 바꿀시 변경 될 변수
+    for index2 in range(데이터 길이 - 1):       # 조건 체크
+        if 앞 데이터 > 뒤 데이터:               # 조건 비교 후 변경
+            데이터 교체(앞 데이터, 뒤 데이터)   # 앞 데이터와 뒤 데이터 변경
+            swap = True                         # 데이터 변경 후 swap 변수를 True로 변경
+
+    if swap == Fasle:                           # swap가 Fasle일 경우 종료(데이터 변경 할것이 없다)
+        break
 '''
-# import random
+import random
 
 
-def bubbiesort(data):
+def bubblesort(data):
     for index in range(len(data) - 1):
         swap = False
         for index2 in range(len(data) - index - 1):
@@ -29,10 +40,14 @@ def bubbiesort(data):
     return data
 
 
-data_list = [17, 6, 8, 11, 2, 3]
+'''
+Case 1 - Random 사용하여 케이스 생성
+'''
+data_list = random.sample(range(100), 50)
+print(bubblesort(data_list))
 
-print(bubbiesort(data_list))
-
-# data_list = random.sample(range(100), 50)
-
-# print(bubbiesort(data_list))
+'''
+Case 2 - 지정된 데이터를 사용하여 케이스 생성
+'''
+# data_list = [17, 6, 8, 11, 2, 3]
+# print(bubblesort(data_list))
--- Basic_Sort/Selection_Sort.py
+++ Basic_Sort/Selection_Sort.py
@@ -5,12 +5,19 @@
 2. 해당 최소값을 데이터 맨 앞에 위치한 값과 교체함
 3. 맨 앞의 위치를 뺀 나머지 데이터를 동일한 방법으로 반복함
 
-for stand in range(len(data) - 1):
-    lowest = stand
+0   1   2   3   인덱스번호
+--------------
+5   4   3   1   초기값
+1   4   3   5
+1   3   4   5
+1   3   4   5   정렬값
+
+for stand in range(len(data) - 1):    # stand = 기준점
+    lowest = stand                      # 맨 처음 최소값이 들어갈 변수명
     for index in range(stand + 1, len(data)):
-        if data[lowest] > data[index]:
+        if data[lowest] > data[index]:    # 최소값
             lowest = index
-    swap(lowest, stand)
+    swap(lowest, index)
 '''
 import random
 
@@ -25,9 +32,13 @@ def selection_sort(data):
     return data
 
 
-# data_list = random.sample(range(100), 10)6
-
-# print(selection_sort(data_list))
-
-data_list = [17, 6, 8, 11, 2, 3]
+'''
+Case 1 - Random 사용하여 케이스 생성
+'''
+data_list = random.sample(range(100), 10)
 print(selection_sort(data_list))
+'''
+Case 2 - 지정된 데이터를 사용하여 케이스 생성
+'''
+# data_list = [17, 6, 8, 11, 2, 3]
+# print(selection_sort(data_list))
