e7fad29f684843b8f954cdd1c38a15fa8a836f2a
AnKiWoong <ankiwoong@gmail.com>
2020-02-24 20:21:31 +0900

    master

--- Basic_Sort/insertion_sort.py
+++ Basic_Sort/insertion_sort.py
@@ -8,8 +8,33 @@
 --------------
 5   3   2   4   초기값
 2   3   5   4
+2   3   4   5
 2   3   4   5   정렬값
 
+데이터길이      조건체크      턴
+2               1             1
+3               2             2
+4               3             3
 
-
+for index in range(데이터 길이 - 1):                   # 턴(index = 턴의 횟수)
+    for index2 in range(index + 1, 0, -1):             # 인덱스부터 시작하여 0까지 -1씩 옮겨감
+        if data[index2] < data[index2 -1]:
+            swap(data[index2], data[index2 - 1])
+        else:
+            break
 '''
+import random
+
+
+def insertion_sort(data):
+    for index in range(len(data) - 1):
+        for index2 in range(index + 1, 0, -1):
+            if data[index2] < data[index2 - 1]:
+                data[index2], data[index2 - 1] = data[index2 - 1], data[index2]
+            else:
+                break
+    return data
+
+
+data_list = random.sample(range(100), 50)
+print(insertion_sort(data_list))
